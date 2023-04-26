95dc35fd7c59670888b31c4f85aaaabff88675d4
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-04-26 04:48:45 +0000

    devise設定済

---
+++ app/controllers/public/registrations_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Public::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+ def after_sign_in_path_for(resource)
+   root_path
+ end
+
+
+
+ protected
+
+ def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :name_kana, :email, :phone_number])
+ end
+end
