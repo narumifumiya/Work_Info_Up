27c83598663c8f146c5ede077b20e445f74191e9
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-04-29 06:28:03 +0000

    いいね非同期通信追加

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,17 +1,19 @@
 class Public::FavoritesController < ApplicationController
 
   def create
-    project = Project.find(params[:project_id])
-    favorite = current_user.favorites.new(project_id: project.id)
+    @company = Company.find(params[:company_id])
+    @project = Project.find(params[:project_id])
+    favorite = current_user.favorites.new(project_id: @project.id)
     favorite.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
-    project = Project.find(params[:project_id])
-    favorite = current_user.favorites.find_by(project_id: project.id)
+    @company = Company.find(params[:company_id])
+    @project = Project.find(params[:project_id])
+    favorite = current_user.favorites.find_by(project_id: @project.id)
     favorite.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
 end
