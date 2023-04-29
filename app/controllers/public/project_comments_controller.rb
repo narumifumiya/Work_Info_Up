e3650874e2cb203e4708234792a8e8dff5cd72ea
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-04-29 06:59:19 +0000

    コメント機能非同期通信化

--- app/controllers/public/project_comments_controller.rb
+++ app/controllers/public/project_comments_controller.rb
@@ -1,16 +1,19 @@
 class Public::ProjectCommentsController < ApplicationController
 
   def create
-    project = Project.find(params[:project_id])
+    @company = Company.find(params[:company_id])
+    @project = Project.find(params[:project_id])
     comment = current_user.project_comments.new(project_comment_params)
-    comment.project_id = project.id
+    comment.project_id = @project.id
     comment.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
+    @company = Company.find(params[:company_id])
+    @project = Project.find(params[:project_id])
     ProjectComment.find(params[:id]).destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   private
