bbb21788eb86e7e75f932c77078e32b94f7e0b26
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-05-01 09:23:15 +0000

    public/project,customerフォームを部分テンプレート化

--- app/models/office.rb
+++ app/models/office.rb
@@ -3,5 +3,10 @@ class Office < ApplicationRecord
   belongs_to :company
 
   validates :name, presence: true
+  
+  # 郵便番号と住所を同時に表示
+  def total_address
+    "〒#{post_code}　#{address}"
+  end
 
 end
