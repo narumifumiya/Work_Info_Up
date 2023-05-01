3314e9bef457646abc0c76689d0d011a34673fd7
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-05-01 16:49:08 +0000

    admin/部分テンプレート完成

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,19 +1,19 @@
 class Admin::SearchesController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def search
     @range = params[:range]
     @word = params[:word]
     @search = params[:search]
 
 
-    if @range == "User"
+    if @range == "ユーザー"
       @users = User.looks(@search, @word)
-    elsif @range == "Company"
+    elsif @range == "得意先"
       @companies = Company.looks(@search, @word)
-    elsif @range == "Project"
+    elsif @range == "プロジェクト"
       @projects = Project.looks(@search, @word)
-    elsif @range == "Tag"
+    elsif @range == "タグ"
       @tags = Tag.looks(@search, @word)
     end
   end
