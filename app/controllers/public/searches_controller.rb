2b74ebba2c6a2abee0de9746d3a5b9faa3335519
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-05-01 09:58:20 +0000

    public側部分テンプレート完成

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -5,15 +5,15 @@ class Public::SearchesController < ApplicationController
     @range = params[:range]
     @word = params[:word]
     @search = params[:search]
-    
 
-    if @range == "User"
+
+    if @range == "ユーザー"
       @users = User.looks(@search, @word)
-    elsif @range == "Company"
+    elsif @range == "得意先"
       @companies = Company.looks(@search, @word)
-    elsif @range == "Project"
+    elsif @range == "プロジェクト"
       @projects = Project.looks(@search, @word)
-    elsif @range == "Tag"
+    elsif @range == "タグ"
       @tags = Tag.looks(@search, @word)
     end
   end
