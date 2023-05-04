0cd22c13a38b75cf207a3eafbe60e4322759323d
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-05-04 16:44:09 +0000

    トップページ調整中

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,8 +2,10 @@
 
 class Admin::SessionsController < Devise::SessionsController
 
-  def after_sign_out_path_for(resource)
-    new_admin_session_path
+  def after_sign_in_path_for(resource)
+    admin_users_path
   end
+
+  
 
 end
