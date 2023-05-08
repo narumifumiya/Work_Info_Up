3ccea09f046aed0c9f4e60794f7ec78a3c013b74
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-05-08 10:49:41 +0000

    production.rb修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
