50faee5676c784c8dd7ff90ab8d2793e15dc9192
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-05-11 10:24:13 +0000

    通知修正

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -1,16 +1,16 @@
 class Public::NotificationsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @notifications = current_user.passive_notifications.page(params[:page])
     @notifications.where(checked: false).each do |notification|
       notification.update(checked: true)
     end
   end
-  
+
   def destroy_all
 		@notifications = current_user.passive_notifications.destroy_all
 		redirect_to notifications_path
-	end
-  
+  end
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -96,7 +96,7 @@ class Project < ApplicationRecord
       save_notification_comment!(current_user, project_comment_id, temp_id['user_id'])
     end
     # まだ誰もコメントしていない場合は、投稿者に通知を送る
-    save_notification_comment!(current_user, project_comment_id, user_id) if temp_ids.blank?
+   save_notification_comment!(current_user, project_comment_id, user_id) 
   end
   
   def save_notification_comment!(current_user, project_comment_id, visited_id)
