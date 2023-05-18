4302f50768bec9a8ed3bd204ab795f61da540c74
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-05-18 07:10:40 +0000

    コメントエリア修正

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,6 +1,9 @@
 class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def top
+    @users = User.all.order(created_at: :desc).limit(6)
+    @companies = Company.all.order(created_at: :desc).limit(6)
+    @projects = Project.all.order(created_at: :desc).limit(6)
   end
 end
