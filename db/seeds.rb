f137c6c075a8971e2ab22d7ad29fe04fdfd98d27
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-05-19 10:19:23 +0000

    seed編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -99,93 +99,93 @@ Project.create!(
     {company_id: 3, user_id: 12, name: '某マンション新築工事',start_date: 'Mon, 01 Jan 2024', end_date: 'Mon, 31 Mar 2025', introduction: '今月中に受注が決まりそうです',order_status: "under_negotiation", progress_status: "not_started_yet" }
   ]
 )
-# いいね
-Favorite.create!(
-  [
-    {project_id: 1, user_id: 2},
-    {project_id: 2, user_id: 2},
-    {project_id: 4, user_id: 3},
-    {project_id: 6, user_id: 3},
-    {project_id: 6, user_id: 7},
-    {project_id: 7, user_id: 4},
-    {project_id: 9, user_id: 4},
-    {project_id: 9, user_id: 8},
-  ]
-)
-# コメント
-ProjectComment.create!(
-  [
-    {project_id: 2, user_id: 2, comment: "受注出来そうですか？"},
-    {project_id: 2, user_id: 10, comment: "来週中に決まりそうです"},
-    {project_id: 4, user_id: 3, comment: "工事終了お疲れ様です"},
-    {project_id: 6, user_id: 3, comment: "営業へ行く際は私も同行しますので、お声がけ下さい"},
-    {project_id: 6, user_id: 7, comment: "手伝えることがあれば言って下さい"},
-    {project_id: 7, user_id: 4, comment: "作業は順調ですか？"},
-    {project_id: 9, user_id: 4, comment: "良いですね！"},
-    {project_id: 9, user_id: 8, comment: "この調子で頑張ってください！"},
-  ]
-)
-# 通知
-Notification.create!(
-  [
-    {visitor_id: 2, visited_id: 10, project_id: 1,action: "favorite" ,checked: false },
-    {visitor_id: 2, visited_id: 10, project_id: 2,action: "favorite" ,checked: false },
-    {visitor_id: 2, visited_id: 10, project_id: 2, project_comment_id: 1,action: "comment" ,checked: false },
-    {visitor_id: 10, visited_id: 2, project_id: 2, project_comment_id: 2,action: "comment" ,checked: false },
-    {visitor_id: 10, visited_id: 10, project_id: 2, project_comment_id: 2,action: "comment" ,checked: true },
-    {visitor_id: 3, visited_id: 7, project_id: 4,action: "favorite" ,checked: false },
-    {visitor_id: 3, visited_id: 7, project_id: 4, project_comment_id: 3, action: "comment" ,checked: false },
-    {visitor_id: 3, visited_id: 11, project_id: 6,action: "favorite" ,checked: false },
-    {visitor_id: 3, visited_id: 11, project_id: 6, project_comment_id: 4,action: "comment" ,checked: false },
-    {visitor_id: 7, visited_id: 11, project_id: 6,action: "favorite" ,checked: false },
-    {visitor_id: 7, visited_id: 3, project_id: 6, project_comment_id: 5, action: "comment" ,checked: false },
-    {visitor_id: 7, visited_id: 11, project_id: 6, project_comment_id: 5, action: "comment" ,checked: false },
-    {visitor_id: 4, visited_id: 8, project_id: 7,action: "favorite" ,checked: false },
-    {visitor_id: 4, visited_id: 8, project_id: 7, project_comment_id: 6,action: "comment" ,checked: false },
-    {visitor_id: 4, visited_id: 12, project_id: 9,action: "favorite" ,checked: false },
-    {visitor_id: 4, visited_id: 12, project_id: 9, project_comment_id: 7,action: "comment" ,checked: false },
-    {visitor_id: 8, visited_id: 12, project_id: 9,action: "favorite" ,checked: false },
-    {visitor_id: 8, visited_id: 4, project_id: 9, project_comment_id: 8,action: "comment" ,checked: false },
-    {visitor_id: 8, visited_id: 12, project_id: 9, project_comment_id: 8,action: "comment" ,checked: false },
-  ]
-)
-# グループ
-Group.create!(
-  [
-    {name: '部長会', introduction: '各営業部の部長同士で情報交換しましょう！', owner_id: 2},
-    {name: '若手会', introduction: '入社１〜３年目社員専用', owner_id: 10}
-  ]
-)
-# グループユーザー
-GroupUser.create!(
-  [
-    {user_id: 2, group_id: 1},
-    {user_id: 3, group_id: 1},
-    {user_id: 4, group_id: 1},
-    {user_id: 5, group_id: 1},
-    {user_id: 10, group_id: 2},
-    {user_id: 11, group_id: 2},
-    {user_id: 12, group_id: 2},
-    {user_id: 13, group_id: 2},
-  ]
-)
-# チャット
-Chat.create!(
-  [
-    {user_id: 2, group_id: 1, message: '第一営業部では今月の予算達成が厳しい見込みです。'},
-    {user_id: 4, group_id: 1, message: '第三営業部で売上を大幅に超える予定ですので、補填可能です。'},
-    {user_id: 2, group_id: 1, message: '大正さん、ありがとうございます。'},
-    {user_id: 3, group_id: 1, message: '第二営業部は予定通りです。'},
-    {user_id: 5, group_id: 1, message: '第四営業部も予定通りとなります。'},
-    {user_id: 10, group_id: 2, message: '勉強会をしませんか？'},
-    {user_id: 11, group_id: 2, message: '良いですね！やりましょう。'},
-    {user_id: 12, group_id: 2, message: '金曜日の午後は予定が空いています。'},
-    {user_id: 13, group_id: 2, message: '金曜日は１５時から参加可能です。'},
-    {user_id: 11, group_id: 2, message: '私も金曜日の午後は大丈夫です。'},
-    {user_id: 10, group_id: 2, message: 'それでは金曜日の１５時からにしたいと思います。'},
-    {user_id: 10, group_id: 2, message: '場所は多目的ルームでお願いします。'},
-  ]
-)
+# # いいね
+# Favorite.create!(
+#   [
+#     {project_id: 1, user_id: 2},
+#     {project_id: 2, user_id: 2},
+#     {project_id: 4, user_id: 3},
+#     {project_id: 6, user_id: 3},
+#     {project_id: 6, user_id: 7},
+#     {project_id: 7, user_id: 4},
+#     {project_id: 9, user_id: 4},
+#     {project_id: 9, user_id: 8},
+#   ]
+# )
+# # コメント
+# ProjectComment.create!(
+#   [
+#     {project_id: 2, user_id: 2, comment: "受注出来そうですか？"},
+#     {project_id: 2, user_id: 10, comment: "来週中に決まりそうです"},
+#     {project_id: 4, user_id: 3, comment: "工事終了お疲れ様です"},
+#     {project_id: 6, user_id: 3, comment: "営業へ行く際は私も同行しますので、お声がけ下さい"},
+#     {project_id: 6, user_id: 7, comment: "手伝えることがあれば言って下さい"},
+#     {project_id: 7, user_id: 4, comment: "作業は順調ですか？"},
+#     {project_id: 9, user_id: 4, comment: "良いですね！"},
+#     {project_id: 9, user_id: 8, comment: "この調子で頑張ってください！"},
+#   ]
+# )
+# # 通知
+# Notification.create!(
+#   [
+#     {visitor_id: 2, visited_id: 10, project_id: 1,action: "favorite" ,checked: false },
+#     {visitor_id: 2, visited_id: 10, project_id: 2,action: "favorite" ,checked: false },
+#     {visitor_id: 2, visited_id: 10, project_id: 2, project_comment_id: 1,action: "comment" ,checked: false },
+#     {visitor_id: 10, visited_id: 2, project_id: 2, project_comment_id: 2,action: "comment" ,checked: false },
+#     {visitor_id: 10, visited_id: 10, project_id: 2, project_comment_id: 2,action: "comment" ,checked: true },
+#     {visitor_id: 3, visited_id: 7, project_id: 4,action: "favorite" ,checked: false },
+#     {visitor_id: 3, visited_id: 7, project_id: 4, project_comment_id: 3, action: "comment" ,checked: false },
+#     {visitor_id: 3, visited_id: 11, project_id: 6,action: "favorite" ,checked: false },
+#     {visitor_id: 3, visited_id: 11, project_id: 6, project_comment_id: 4,action: "comment" ,checked: false },
+#     {visitor_id: 7, visited_id: 11, project_id: 6,action: "favorite" ,checked: false },
+#     {visitor_id: 7, visited_id: 3, project_id: 6, project_comment_id: 5, action: "comment" ,checked: false },
+#     {visitor_id: 7, visited_id: 11, project_id: 6, project_comment_id: 5, action: "comment" ,checked: false },
+#     {visitor_id: 4, visited_id: 8, project_id: 7,action: "favorite" ,checked: false },
+#     {visitor_id: 4, visited_id: 8, project_id: 7, project_comment_id: 6,action: "comment" ,checked: false },
+#     {visitor_id: 4, visited_id: 12, project_id: 9,action: "favorite" ,checked: false },
+#     {visitor_id: 4, visited_id: 12, project_id: 9, project_comment_id: 7,action: "comment" ,checked: false },
+#     {visitor_id: 8, visited_id: 12, project_id: 9,action: "favorite" ,checked: false },
+#     {visitor_id: 8, visited_id: 4, project_id: 9, project_comment_id: 8,action: "comment" ,checked: false },
+#     {visitor_id: 8, visited_id: 12, project_id: 9, project_comment_id: 8,action: "comment" ,checked: false },
+#   ]
+# )
+# # グループ
+# Group.create!(
+#   [
+#     {name: '部長会', introduction: '各営業部の部長同士で情報交換しましょう！', owner_id: 2},
+#     {name: '若手会', introduction: '入社１〜３年目社員専用', owner_id: 10}
+#   ]
+# )
+# # グループユーザー
+# GroupUser.create!(
+#   [
+#     {user_id: 2, group_id: 1},
+#     {user_id: 3, group_id: 1},
+#     {user_id: 4, group_id: 1},
+#     {user_id: 5, group_id: 1},
+#     {user_id: 10, group_id: 2},
+#     {user_id: 11, group_id: 2},
+#     {user_id: 12, group_id: 2},
+#     {user_id: 13, group_id: 2},
+#   ]
+# )
+# # チャット
+# Chat.create!(
+#   [
+#     {user_id: 2, group_id: 1, message: '第一営業部では今月の予算達成が厳しい見込みです。'},
+#     {user_id: 4, group_id: 1, message: '第三営業部で売上を大幅に超える予定ですので、補填可能です。'},
+#     {user_id: 2, group_id: 1, message: '大正さん、ありがとうございます。'},
+#     {user_id: 3, group_id: 1, message: '第二営業部は予定通りです。'},
+#     {user_id: 5, group_id: 1, message: '第四営業部も予定通りとなります。'},
+#     {user_id: 10, group_id: 2, message: '勉強会をしませんか？'},
+#     {user_id: 11, group_id: 2, message: '良いですね！やりましょう。'},
+#     {user_id: 12, group_id: 2, message: '金曜日の午後は予定が空いています。'},
+#     {user_id: 13, group_id: 2, message: '金曜日は１５時から参加可能です。'},
+#     {user_id: 11, group_id: 2, message: '私も金曜日の午後は大丈夫です。'},
+#     {user_id: 10, group_id: 2, message: 'それでは金曜日の１５時からにしたいと思います。'},
+#     {user_id: 10, group_id: 2, message: '場所は多目的ルームでお願いします。'},
+#   ]
+# )
 # タグ
 Tag.create!(
   [
